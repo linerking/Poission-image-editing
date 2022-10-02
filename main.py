283d0156c8348316d10bb0824db067ebbcfd1a72
linerking <117010177@link.cuhk.edu.cn>
2022-10-02 20:20:15 +0800

    finish main

--- main.py
+++ main.py
@@ -1,27 +1,29 @@
 '''
 Author: Yihan Liu
 Date: 2022-10-01 21:14:31
-LastEditTime: 2022-10-01 22:06:28
+LastEditTime: 2022-10-02 20:19:56
 Email: 117010177@link.cuhk.edu.cn
 '''
 import numpy as np
+import sys
+sys.path.append("./src")
 import Poisson as ps
 import cv2
-import sys
+
 
 # Prepare the input images 
 argvs = sys.argv
 src_name = argvs[1]
 mask_name = argvs[2]
 target_name = argvs[3]
-res_in = "../res/Input/"
+res_in = "./res/Input/"
 pic_type = ".jpeg"
 src_path =  res_in + src_name + pic_type
 mask_path = res_in + mask_name +pic_type
 target_path = res_in + target_name + pic_type
 
 # Prepare the output file
-res_out = "../res/Output/"
+res_out = "./res/Output/"
 out_path = res_out + "result.jpeg"
 
 # load images
@@ -31,3 +33,8 @@ mask = np.array(cv2.imread(mask_path, 0), dtype=np.uint8)
 ret, mask = cv2.threshold(mask, 0, 255, cv2.THRESH_OTSU)
 mask = mask/255.0
 
+# Solve Poisson equation 
+result = ps.Poisson(src,mask,target)
+
+# Store the result
+cv2.imwrite(out_path,result)
--- src/Poisson.py
+++ src/Poisson.py
@@ -1,6 +1,8 @@
 '''
 Author: Yihan Liu
 Date: 2022-10-01 21:03:29
-LastEditTime: 2022-10-01 21:05:25
+LastEditTime: 2022-10-02 20:13:18
 Email: 117010177@link.cuhk.edu.cn
 '''
+def Poisson(src,mask,target):
+    return "test"
