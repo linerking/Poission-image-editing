afe7a16038f3608dba52e3234b15ccce5fafbb8d
linerking <117010177@link.cuhk.edu.cn>
2022-10-03 00:13:18 +0800

    version1

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 '''
 Author: Yihan Liu
 Date: 2022-10-01 21:14:31
-LastEditTime: 2022-10-02 20:19:56
+LastEditTime: 2022-10-03 00:12:39
 Email: 117010177@link.cuhk.edu.cn
 '''
 import numpy as np
@@ -21,20 +21,19 @@ pic_type = ".jpeg"
 src_path =  res_in + src_name + pic_type
 mask_path = res_in + mask_name +pic_type
 target_path = res_in + target_name + pic_type
-
+place_to_put = [80,150]
 # Prepare the output file
 res_out = "./res/Output/"
 out_path = res_out + "result.jpeg"
 
 # load images
-src = np.array(cv2.imread(src_path, 1)/255.0, dtype=np.float32)
-target = np.array(cv2.imread(target_path, 1)/255.0, dtype=np.float32)
+src = np.array(cv2.imread(src_path, 1), dtype=np.float32)
+target = np.array(cv2.imread(target_path, 1), dtype=np.float32)
 mask = np.array(cv2.imread(mask_path, 0), dtype=np.uint8)
 ret, mask = cv2.threshold(mask, 0, 255, cv2.THRESH_OTSU)
 mask = mask/255.0
-
 # Solve Poisson equation 
-result = ps.Poisson(src,mask,target)
+result = ps.Poisson(src,mask,target,place_to_put)
 
 # Store the result
 cv2.imwrite(out_path,result)
--- src/Poisson.py
+++ src/Poisson.py
@@ -1,8 +1,65 @@
 '''
 Author: Yihan Liu
 Date: 2022-10-01 21:03:29
-LastEditTime: 2022-10-02 20:13:18
+LastEditTime: 2022-10-03 00:03:04
 Email: 117010177@link.cuhk.edu.cn
 '''
-def Poisson(src,mask,target):
-    return "test"
+import numpy as np
+import cv2
+from scipy.sparse import lil_matrix as lil_matrix
+from scipy.sparse import linalg
+
+
+np.set_printoptions(threshold=np.inf)
+
+def get_neighbor(index):
+    i,j = index
+    return [(i+1,j),(i-1,j),(i,j+1),(i,j-1)]
+def create_lapla_matrix(N,omega):
+    A = lil_matrix((N,N))
+    for i,index in enumerate(omega):
+        A[i,i] = 4
+        for x in get_neighbor(index):
+            if x not in omega: continue
+            j = omega.index(x)
+            A[i,j] = -1
+    return A
+def find_min(a):
+    buf = 100000
+    for i in a:
+        if i < buf:
+            buf = i
+    return buf
+def calculate_lapla(index,src):
+    i,j = index
+    lapla = (4 * src[i][j]) - src[i+1][j] - src[i-1][j] - src[i][j+1] - src[i][j-1]
+    return lapla
+def Poisson(src,mask,target,place_to_put):
+    omega_src = np.nonzero(mask)
+    top_left = [omega_src[0][0],find_min(omega_src[1])]
+    offset = [place_to_put[0]-top_left[0],place_to_put[1]-top_left[1]]
+    omega_src = zip(omega_src[0],omega_src[1])
+    a = list(omega_src)
+    pix_in_omega = len(a)
+    # matrix for calculate the laplacian of f
+    Lapla_f_A = create_lapla_matrix(pix_in_omega,a)
+    result = []
+    # handle the pic channel by channel
+    for i in range(3):
+        print(1)
+        src_channel = src[:,:,i]
+        target_channel = target[:,:,i]
+        Lapla_g = np.zeros(pix_in_omega)
+        for j,index in enumerate(a):
+            Lapla_g[j] = calculate_lapla(index,src_channel)
+            for n in get_neighbor(index):
+                if mask[n] == 0:
+                    x,y = n
+                    Lapla_g[j] += target_channel[x+offset[0]][y+offset[1]]
+        m = linalg.cg(Lapla_f_A, Lapla_g)
+        result.append(np.copy(target_channel))
+        for j,index in enumerate(a):
+            x,y = index
+            result[i][x+offset[0]][y+offset[1]] = m[0][j]
+    result = cv2.merge(result)
+    return result
